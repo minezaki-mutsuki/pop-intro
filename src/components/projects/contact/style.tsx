4408f06500ecb870fd6103ae0fd6d1956110f72d
Mutsuki Minezaki <m.minezaki@4nonome.com>
2022-12-30 01:57:09 +0900

    projects/contactの作成

---
+++ src/components/projects/contact/style.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: grid;
+    gap: 10px;
+`;
+export const ButtonWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+`
