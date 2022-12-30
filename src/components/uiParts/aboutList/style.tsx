8cc34928b873eb7f8928baa8f5936471f45e02e5
Mutsuki Minezaki <m.minezaki@4nonome.com>
2022-12-30 15:14:00 +0900

    aboutListの修正

--- src/components/uiParts/aboutList/style.tsx
+++ src/components/uiParts/aboutList/style.tsx
@@ -3,6 +3,20 @@ import styled from "styled-components";
 export const ListWrapper = styled.div`
     
 `;
+export const TitleText = styled.p`
+    width: 25%;
+    margin: 0;
+    border-right: solid 1px #0E0841;
+    padding: 10px;
+    font-weight: 700;
+    color: #0E0841;
+`;
+export const ExplanationText = styled.p`
+    width: 75%;
+    color: #0E0841;
+    margin: 0;
+    padding: 10px;
+`;
 export const Wrapper = styled.div`
     max-width: 600px;
     display: flex;
@@ -10,7 +24,13 @@ export const Wrapper = styled.div`
     border-bottom: none;
     border-top: none;
     &:nth-child(2n+1) {
-        background-color: #DCD8FF;
+        background-color: #0E0841;
+        ${TitleText} {
+            color: white;
+        };
+        ${ExplanationText} {
+            color: white;
+        };
     }
     &:first-child {
         border-top: solid 5px #0E0841;
@@ -22,16 +42,4 @@ export const Wrapper = styled.div`
         border-bottom-left-radius: 12px;
         border-bottom-right-radius: 12px;
     }
-`;
-export const TitleText = styled.p`
-    width: 25%;
-    margin: 0;
-    border-right: solid 1px #0E0841;
-    padding: 10px;
-    font-weight: 700;
-`;
-export const ExplanationText = styled.p`
-    width: 75%;
-    margin: 0;
-    padding: 10px;
 `;
