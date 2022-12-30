1bb4f53f8ea0e8356a4b47af2b4da53715ab41eb
Mutsuki Minezaki <m.minezaki@4nonome.com>
2022-12-30 14:46:44 +0900

    topの修正

--- src/components/projects/top/style.tsx
+++ src/components/projects/top/style.tsx
@@ -2,13 +2,11 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     background-color: black;
-    max-width: 600px;
-    padding: 30px;
-    border-radius: 12px;
+    padding: 90px 30px 30px;
     margin: auto;
 `;
 export const TopText = styled.p`
-    font-size: 36px;
+    font-size: 28px;
     font-weight: 700;
     color: white;
     text-align: center;
