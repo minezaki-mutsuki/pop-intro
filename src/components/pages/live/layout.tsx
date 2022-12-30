1e5097fb3da1b93a2c174c92953ae9337fb25a50
Mutsuki Minezaki <m.minezaki@4nonome.com>
2022-12-30 16:14:12 +0900

    pages/liveの作成

---
+++ src/components/pages/live/layout.tsx
@@ -0,0 +1,40 @@
+import { Footer } from "../../projects/footer";
+import { Header } from "../../projects/header";
+import { Button } from "../../uiParts/button";
+import { ButtonWrapper, LiveDay, Wrapper } from "./style";
+
+type LiveItems = {
+    day: string;
+    link: string;
+    name: string;
+}
+
+type LiveProps = {
+    liveList: LiveItems[];
+}
+
+export const LiveLayout = ({liveList}: LiveProps) => {
+    const onClickToTop = () => {
+        console.log("トップに遷移");
+        //TODO : useNavigate使いたい
+    };
+
+    return (
+        <Wrapper>
+            < Header/>
+            <div>
+            {liveList.map((item, index) => (
+                <>
+                    <LiveDay key={index}>{item.day}</LiveDay>                    <a href={item.link}>
+                    <Button text={item.name} onClick={() => console.log(item.link)} />
+                </a>
+                </>
+            ))}
+            </div>
+            <ButtonWrapper>
+                <Button text={"トップに戻る"} onClick={onClickToTop} />
+            </ButtonWrapper>
+            <Footer />
+        </Wrapper>
+    );
+}
