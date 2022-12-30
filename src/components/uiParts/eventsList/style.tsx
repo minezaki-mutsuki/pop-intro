c5cb8490387568faac7697168b5202196698047c
Mutsuki Minezaki <m.minezaki@4nonome.com>
2022-12-30 15:18:34 +0900

    eventsListの修正

--- src/components/uiParts/eventsList/style.tsx
+++ src/components/uiParts/eventsList/style.tsx
@@ -4,6 +4,16 @@ export const ListWrapper = styled.div`
     max-width: 600px;
     border-radius: 12px;
 `;
+export const DayText = styled.time`
+    font-size: 12px;
+    color: #645ABE;
+    font-weight: 700;
+`;
+export const ExplanationText = styled.p`
+    margin: 0;
+    font-weight: 700;
+    color: #0E0841;
+`;
 export const Wrapper = styled.div`
     display: grid;
     gap: 10px;
@@ -11,7 +21,13 @@ export const Wrapper = styled.div`
     border-right: solid 5px #0E0841;
     border-left: solid 5px #0E0841;
     &:nth-child(2n+1) {
-        background-color: #DCD8FF;
+        background-color: #0E0841;
+        ${ExplanationText} {
+            color: white;
+        };
+        ${DayText} {
+            color: white;
+        };
     }
     &:first-child {
         border-top: solid 5px #0E0841;
@@ -23,13 +39,4 @@ export const Wrapper = styled.div`
         border-bottom-left-radius: 12px;
         border-bottom-right-radius: 12px;
     }
-`;
-export const DayText = styled.time`
-    font-size: 12px;
-    color: #645ABE;
-    font-weight: 700;
-`;
-export const ExplanationText = styled.p`
-    margin: 0;
-    font-weight: 700;
 `;
