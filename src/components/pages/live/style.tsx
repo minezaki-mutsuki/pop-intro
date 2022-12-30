1ce2444462a15bc210e0f6f2045ab6870c94ad61
Mutsuki Minezaki <m.minezaki@4nonome.com>
2022-12-30 22:35:08 +0900

    padding設定

--- src/components/pages/live/style.tsx
+++ src/components/pages/live/style.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
     display: grid;
     gap: 50px;
-    padding-top: 50px;
+    padding: 50px 30px 0;
 `;
 export const ButtonWrapper = styled.div`
     display: flex;
