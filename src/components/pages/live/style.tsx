a59edb112889d3024bb3465db43fefa726d62fc0
mutsuki <mutu141210@gmail.com>
2023-04-07 12:22:16 +0900

    style修正

--- src/components/pages/live/style.tsx
+++ src/components/pages/live/style.tsx
@@ -1,19 +1,19 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    display: grid;
-    gap: 50px;
-    padding-top: 50px;
+  display: grid;
+  gap: 50px;
+  padding-top: 50px;
 `;
 export const ButtonWrapper = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    padding: 0 30px;
+  display: flex;
+  justify-content: flex-end;
+  padding: 0 30px;
 `;
 export const BodyWrapper = styled.div`
-    padding: 0 30px;
+  padding: 30px;
 `;
 export const LiveDay = styled.p`
-    margin: 20px 0 10px;
-    font-weight: 700;
+  margin: 20px 0 10px;
+  font-weight: 700;
 `;
